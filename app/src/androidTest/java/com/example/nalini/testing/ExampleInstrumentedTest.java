996caa8842adf62c4efb5f1dc180ef3600b7e80a
nalinisaxena <nalinisaxena13@gmail.com>
2017-01-09 22:43:11 +0530

    to test git

--- app/src/androidTest/java/com/example/nalini/testing/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/nalini/testing/ExampleInstrumentedTest.java
@@ -22,5 +22,6 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.nalini.testing", appContext.getPackageName());
+        //to test github
     }
 }
