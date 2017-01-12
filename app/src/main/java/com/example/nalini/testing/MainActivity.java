aeb56465d222d239421e8707676ed0fe7d40ba79
neha4223 <yad.neha1989@gmail.com>
2017-01-12 19:18:05 +0530

    commited

--- app/src/main/java/com/example/nalini/testing/MainActivity.java
+++ app/src/main/java/com/example/nalini/testing/MainActivity.java
@@ -10,12 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //hello
 
-
-        //blood finder
-
-        //hiii baby
-//hiiii pagal
     }
 }
